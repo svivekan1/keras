a8e514c73f87b8e0b190fd14fe14e2f41280941b
svivekan <sanjayvivek@hotmail.com>
2017-12-15 13:34:18 +0000

    updated

--- examples/mnist_cnn.py
+++ examples/mnist_cnn.py
@@ -2,7 +2,7 @@
 
 Gets to 99.25% test accuracy after 12 epochs
 (there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU..
+16 seconds per epoch on a GRID K520 GPU.
 '''
 
 from __future__ import print_function
